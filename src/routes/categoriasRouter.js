5cd672cade7d05e1c2e4ef0c01e17a10937f70a1
alezi-n <study.alez@gmail.com>
2024-02-01 23:40:35 -0300

    primeiro requisito

--- src/routes/categoriasRouter.js
+++ src/routes/categoriasRouter.js
@@ -5,10 +5,10 @@ const categoriasController = new CategoriaController();
 
 const router = Router();
 
-router.get('/categoria', (req, res) => categoriasController.pegaTodos(req, res));
-router.get('/categoria/:id', (req, res) => categoriasController.pegaUmPorId(req, res));
-router.post('/categoria', (req, res) => categoriasController.criaNovo(req, res));
-router.put('/categoria/:id', (req, res) => categoriasController.atualiza(req, res));
-router.delete('/categoria/:id', (req, res) => categoriasController.exclui(req, res));
+router.get('/categorias', (req, res) => categoriasController.pegaTodos(req, res));
+router.get('/categorias/:id', (req, res) => categoriasController.pegaUmPorId(req, res));
+router.post('/categorias', (req, res) => categoriasController.criaNovo(req, res));
+router.put('/categorias/:id', (req, res) => categoriasController.atualiza(req, res));
+router.delete('/categorias/:id', (req, res) => categoriasController.exclui(req, res));
 
 module.exports = router;
--- src/routes/cursosRouter.js
+++ src/routes/cursosRouter.js
@@ -5,10 +5,10 @@ const cursosController = new CursoController();
 
 const router = Router();
 
-router.get('/curso', (req, res) => cursosController.pegaTodos(req, res));
-router.get('/curso/:id', (req, res) => cursosController.pegaUmPorId(req, res));
-router.post('/curso', (req, res) => cursosController.criaNovo(req, res));
-router.put('/curso/:id', (req, res) => cursosController.atualiza(req, res));
-router.delete('/curso/:id', (req, res) => cursosController.exclui(req, res));
+router.get('/cursos', (req, res) => cursosController.pegaTodos(req, res));
+router.get('/cursos/:id', (req, res) => cursosController.pegaUmPorId(req, res));
+router.post('/cursos', (req, res) => cursosController.criaNovo(req, res));
+router.put('/cursos/:id', (req, res) => cursosController.atualiza(req, res));
+router.delete('/cursos/:id', (req, res) => cursosController.exclui(req, res));
 
 module.exports = router;
