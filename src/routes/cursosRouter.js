230ec3013d4c8ec42832db29cec58fcfef1a312b
alezi-n <study.alez@gmail.com>
2024-02-02 23:52:40 -0300

    adicionando operadores

--- src/routes/cursosRouter.js
+++ src/routes/cursosRouter.js
@@ -5,10 +5,12 @@ const cursosController = new CursoController();
 
 const router = Router();
 
-router.get('/cursos', (req, res) => cursosController.pegaTodos(req, res));
+router.get('/cursos', (req, res) => cursosController.pegaCursos(req, res));
 router.get('/cursos/:id', (req, res) => cursosController.pegaUmPorId(req, res));
 router.post('/cursos', (req, res) => cursosController.criaNovo(req, res));
 router.put('/cursos/:id', (req, res) => cursosController.atualiza(req, res));
 router.delete('/cursos/:id', (req, res) => cursosController.exclui(req, res));
 
 module.exports = router;
+
+// ?data_inicial=2023-01-01&data_final=2023-09-01
